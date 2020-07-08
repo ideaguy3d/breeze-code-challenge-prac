18d2a6d0851e16c4cd90dfe52e8072cce772af17
julius <phpninja@mail.com>
2020-07-08 13:28:46 -0700

    trying to understand laravel resources and collections better

--- app/Http/Resources/PersonResource.php
+++ app/Http/Resources/PersonResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PersonResource extends JsonResource
@@ -9,19 +10,19 @@ class PersonResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
+     *
      * @return array
      */
-     public function toArray($request)
- {
-     return [
-         'id'            => $this->id,
-         'first_name'    => $this->first_name,
-         'last_name'     => $this->last_name,
-         'email_address' => $this->email_address,
-         'status'        => $this->status,
-         'created_at'    => $this->created_at,
-         'updated_at'    => $this->updated_at,
-     ];
- }
+    public function toArray($request) {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email_address' => $this->email_address,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
 }
