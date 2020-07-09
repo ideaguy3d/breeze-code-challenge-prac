0b004f51647c7726278c03b14013dea5148a639b
julius <phpninja@mail.com>
2020-07-08 19:22:21 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function() {
     return view('welcome');
 });
+
+Route::get('/file', function() {
+    Log::info('_> seeing if this works');
+    return view('file-practice');
+});
+
+Route::post('upload', 'PracticeFileUploadController@index');
