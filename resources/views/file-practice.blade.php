d3c3e5d881508c68cb2c90492806a0fa09fcd669
julius <phpninja@mail.com>
2020-07-08 19:22:17 -0700

    Create file-practice.blade.php

---
+++ resources/views/file-practice.blade.php
@@ -0,0 +1,7 @@
+<br><br>
+<h1>Upload a file</h1>
+<form action="api/files-one" method="post" enctype="multipart/form-data">
+    csrf()
+    <input type="file" name="jprac">
+    <button type="submit">upload</button>
+</form>
