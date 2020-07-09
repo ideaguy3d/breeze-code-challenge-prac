4958c7bfcc7bc0ba21412fc235fca5e8252a6d7a
julius <phpninja@mail.com>
2020-07-08 19:22:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+// node modules
 import React from "react";
 import ReactDOM from "react-dom";
-import { Container, Header } from "semantic-ui-react";
+import {Container, Header} from "semantic-ui-react";
 
+// custom components
 import ResultsList from "./ResultsList";
+import UploadRail from './UploadRail';
+import PracFileUpload from './PracFileUpload';
 
-const App = ({ children }) => (
-  <Container style={{ margin: 20 }}>
-    <Header as="h3"><span role="img" aria-label="logo">⛵️</span> Breeze Church Management </Header>
+const App = ({children}) => (
+    <Container style={{margin: 20}}>
+        <UploadRail/>
 
-    {children}
-  </Container>
+        {children}
+
+        <br/><br/>
+
+        <h1>Practice file upload component</h1>
+        <PracFileUpload/>
+    </Container>
 );
 
 const styleLink = document.createElement("link");
@@ -18,8 +27,8 @@ styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css
 document.head.appendChild(styleLink);
 
 ReactDOM.render(
-  <App>
-    <ResultsList />
-  </App>,
-  document.getElementById("root")
+    <App>
+        <ResultsList/>
+    </App>,
+    document.getElementById("root")
 );
